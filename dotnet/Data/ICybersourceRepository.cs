7950963150d1e0669e591e337e2ad90b11c8a692
Brian Talma <brian.talma@vtex.com.br>
2022-04-29 15:51:33 -0400

    Analysis synchronous response Store results in vBase

---
+++ dotnet/Data/ICybersourceRepository.cs
@@ -0,0 +1,11 @@
+﻿using Cybersource.Models;
+using System.Threading.Tasks;
+
+namespace Cybersource.Data
+{
+    public interface ICybersourceRepository
+    {
+        Task<SendAntifraudDataResponse> GetAntifraudData(string id);
+        Task SaveAntifraudData(string id, SendAntifraudDataResponse antifraudDataResponse);
+    }
+}
